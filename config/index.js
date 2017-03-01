ef889be3744873ca582f06e035f6e5b7c95acd8e
coolfish <461836324@qq.com>
2017-03-01 12:18:42 +0800

    获取用户信息

--- config/index.js
+++ config/index.js
@@ -1,4 +1,9 @@
 module.exports = {
+    HOST: 'http://127.0.0.1:9018',
     PORT: 9018,
-    MONGO_URL: 'mongodb://127.0.0.1:27017/coding'
+    MONGO_URL: 'mongodb://127.0.0.1:27017/coding',
+    GITHUB: {
+        CLIENTID: 'f6fbdf06449f3727af97',
+        CLIENTSECRET: 'a873e58f400e7a1b162f76cb7f09fa340bb46611'
+    }
 }
